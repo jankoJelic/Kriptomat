267c33852911dc7fb3caf5d5433c5f911b1ea30c
Janko Jelic <janko.jelic99@gmail.com>
2022-07-29 23:57:55 +0200

    iOS status bar

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import {KeyboardAvoidingView, StatusBar} from 'react-native';
 import appStyles from './src/constants/appStyles';
 import RootNavigator from './src/navigation/RootNavigator';
+import MyStatusBar from './src/components/MyStatusBar';
 
 const App = () => {
   return (
     <KeyboardAvoidingView
       style={{flex: 1, backgroundColor: appStyles.colors.backgroundMain}}>
-      <StatusBar backgroundColor={appStyles.colors.brand} />
+      <MyStatusBar />
+      {/* <StatusBar backgroundColor={appStyles.colors.brand} /> */}
       <RootNavigator />
     </KeyboardAvoidingView>
   );
---
+++ src/components/MyStatusBar.tsx
@@ -0,0 +1,43 @@
+import appStyles from 'constants/appStyles';
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  Platform,
+  SafeAreaView,
+} from 'react-native';
+
+const MyStatusBar = () => (
+  <View style={[styles.statusBar, {backgroundColor: appStyles.colors.brand}]}>
+    <SafeAreaView>
+      <StatusBar
+        translucent
+        backgroundColor={appStyles.colors.brand}
+        barStyle="light-content"
+      />
+    </SafeAreaView>
+  </View>
+);
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor: '#79B45D',
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#33373B',
+  },
+});
+
+export default MyStatusBar;
