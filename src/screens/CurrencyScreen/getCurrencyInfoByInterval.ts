4c4beaa2935249159a05fb14349a4c6511238f6e
Janko Jelic <janko.jelic99@gmail.com>
2022-07-31 21:32:31 +0200

    Revert "Update getCurrencyInfoByInterval.ts"
    This reverts commit c6b2f2fe21f3c3540c8f1065aaa290b512ac687a.

--- src/screens/CurrencyScreen/getCurrencyInfoByInterval.ts
+++ src/screens/CurrencyScreen/getCurrencyInfoByInterval.ts
@@ -1,12 +1,21 @@
 import store from 'store';
-import {addCurrencyDisplayInfos} from 'store/currencyOverviewSlice';
 
 const getCurrencyInfoByInterval = () => {
   const state = store.getState();
 
   const {pricesData, activeFilter, currencyInfo} = state.currencyOverviewSlice;
 
-  const {id} = currencyInfo;
+  const {
+    id,
+    market_data: {
+      low_24h,
+      high_24h,
+      price_change_percentage_1y,
+      price_change_percentage_30d,
+      price_change_percentage_24h,
+      price_change_percentage_7d,
+    },
+  } = currencyInfo;
 
   const storedPrices = pricesData.find(
     p => p.currencyId === id && p.filterId === activeFilter.id,
@@ -15,21 +24,43 @@ const getCurrencyInfoByInterval = () => {
   if (!!storedPrices) {
     const minValue = Math.min(...storedPrices);
     const maxValue = Math.max(...storedPrices);
-    const currentPrice = storedPrices[storedPrices.length - 1];
-    const firstPriceInInterval = storedPrices[0];
 
-    const percentageChange =
-      ((currentPrice - firstPriceInInterval) / firstPriceInInterval) * 100;
+    const percentageChange = (maxValue / minValue) * 100;
 
-    const data = {
-      id,
-      intervalId: activeFilter.id,
-      low: minValue,
-      high: maxValue,
-      priceChangePercentage: percentageChange,
-    };
-
-    return data;
+    switch (activeFilter.id) {
+      case 1:
+        return {
+          low: low_24h.eur,
+          high: high_24h.eur,
+          priceChangePercentage: price_change_percentage_24h,
+        };
+      case 2:
+        return {
+          low: minValue,
+          high: maxValue,
+          priceChangePercentage: price_change_percentage_7d,
+        };
+      case 3:
+        return {
+          low: minValue,
+          high: maxValue,
+          priceChangePercentage: price_change_percentage_30d,
+        };
+      case 4:
+        return {
+          low: minValue,
+          high: maxValue,
+          priceChangePercentage: price_change_percentage_1y,
+        };
+      case 5:
+        return {
+          low: minValue,
+          high: maxValue,
+          priceChangePercentage: percentageChange,
+        };
+      default:
+        return emptyData;
+    }
   } else {
     return emptyData;
   }
